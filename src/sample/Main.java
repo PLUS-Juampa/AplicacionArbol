05b380f4b4ab7523617aa5abeafc0b9080198706
Juampa <juampa249@gmail.com>
2017-12-04 12:09:50 -0600

    Interfaz

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Control de Árboles");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 300));
         primaryStage.show();
     }
 
