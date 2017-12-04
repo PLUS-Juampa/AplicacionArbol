8f7eea63b72cbcb3f3b5f90180ab2ad5b837e7db
Juampa <juampa249@gmail.com>
2017-12-04 12:46:15 -0600

    Modelo Actualizado

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Control de Árboles");
-        primaryStage.setScene(new Scene(root, 700, 300));
+        primaryStage.setScene(new Scene(root, 725, 520));
         primaryStage.show();
     }
 
